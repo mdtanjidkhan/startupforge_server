952b6de63904db1ba443501768ce62e4e59ab54b
mdtanjidkhan <mdtanjidkhan152@gmail.com>
2026-06-26 12:58:10 +0600

    feat: add patch endpoint to block platform users by toggling isBlocked to true

--- index.js
+++ index.js
@@ -226,7 +226,7 @@ app.delete('/api/opportunities/:id', async (req, res) => {
   }
 });
 
-// STARTUP PROFILE ROUTEs
+// STARTUP PROFILE 
 
 app.get('/api/my-startup', async (req, res) => {
   try {
